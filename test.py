5af159102a481244f11434f8e10255b318a2fe57
Kourosh Kehtari <kouroshkehtari@gmail.com>
2023-09-18 16:10:02 -0600

    request raw

--- test.py
+++ test.py
@@ -11,3 +11,13 @@ try:
     print(response.text)
 except requests.exceptions.RequestException as e:
     print(e)
+
+github_raw_url = "https://raw.githubusercontent.com/kouroshkeh/lab1/master/test.py"
+
+try:
+    response = requests.get(github_raw_url)
+    response.raise_for_status()
+
+    print(response.text)
+except requests.exceptions.RequestException as e:
+    print(e)
